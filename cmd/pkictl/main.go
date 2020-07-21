2a5869f0f11153ebc2e134811d883595581f6f42
Brian Kim <bk@breadtech.com>
2020-07-21 18:05:57 -0400

    messing around with gitmodules for bi support

--- cmd/pkictl/main.go
+++ cmd/pkictl/main.go
@@ -5,10 +5,18 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/BreadTech/pki/cmd/pkictl/generate"
+	"github.com/BreadTech/pki/cmd/pkictl/get"
 	"github.com/BreadTech/pki/cmd/pkictl/hash"
+	"github.com/BreadTech/pki/cmd/pkictl/read"
 	"github.com/BreadTech/pki/cmd/pkictl/sign"
 	"github.com/BreadTech/pki/cmd/pkictl/verify"
+
+	bi "breadtech/interface"
 )
+
+type A struct {
+	bi.Input
+}
 
 func main() {
 	cmd := &cobra.Command{
@@ -17,7 +25,9 @@ func main() {
 		Long:  "A command-line tool for administering public key infrastructure",
 	}
 	cmd.AddCommand(generate.Cmd)
+	cmd.AddCommand(get.Cmd)
 	cmd.AddCommand(hash.Cmd)
+	cmd.AddCommand(read.Cmd)
 	cmd.AddCommand(sign.Cmd)
 	cmd.AddCommand(verify.Cmd)
 	if err := cmd.Execute(); err != nil {
--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module github.com/BreadTech/pki
 
 go 1.13
 
+replace breadtech/interface => ./pkg/bi/go
+
 require (
-	github.com/gofrs/uuid v3.3.0+incompatible
+	breadtech/interface v0.0.1
+	github.com/gofrs/uuid v3.3.0+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.11.0
+	github.com/jinzhu/gorm v1.9.15
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v0.0.0-20200418040025-38ba6e5628f1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
 )
